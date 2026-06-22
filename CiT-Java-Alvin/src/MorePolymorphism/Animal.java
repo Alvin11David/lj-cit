515986f87ebabfbbdd8999eb0b64f2abce997bae
Kainamatsikofaith <faithkainamatsiko@gmail.com>
2026-06-22 12:35:16 +0300

    Tried out polymorphism on the Animal example

---
+++ CiT-Java-Alvin/src/MorePolymorphism/Animal.java
@@ -0,0 +1,7 @@
+package MorePolymorphism;
+
+public class Animal {
+    public void animalSound() {
+        System.out.println("An animal makes a sound.");
+    }
+}
---
+++ CiT-Java-Alvin/src/MorePolymorphism/Dog.java
@@ -0,0 +1,8 @@
+package MorePolymorphism;
+
+public class Dog extends Animal {
+    @Override
+    public void animalSound() {
+        System.out.println("A dog says woof woof!");
+    }
+}
