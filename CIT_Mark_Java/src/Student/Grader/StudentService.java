fb028469f2e7b3f0e7f8996dd3b417f6663ab74c
sekibaalamark <markmajje256@gmail.com>
2026-06-23 09:24:35 +0300

    Add getter methods for student attributes

---
+++ CIT_Mark_Java/src/Student/Grader/StudentService.java
@@ -0,0 +1,18 @@
+package Student.Grader;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class StudentService {
+    HashMap<String,Student> studentHashMap = new HashMap<>();
+
+    public void add(String registrationNumber, Student student){
+        studentHashMap.put(registrationNumber,student);
+    }
+
+    public Student find(String registrationNumber){
+        try {
+            studentHashMap.get(registrationNumber).
+        }
+        catch ()
+    }
+}
