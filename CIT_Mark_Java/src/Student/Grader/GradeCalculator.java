fbce59705b27b5ac9fe5581f308a6ca2291772c4
sekibaalamark <markmajje256@gmail.com>
2026-06-23 13:02:20 +0300

    Create GradeCalculator class to convert int average to string grade

---
+++ CIT_Mark_Java/src/Student/Grader/GradeCalculator.java
@@ -0,0 +1,25 @@
+package Student.Grader;
+
+public class GradeCalculator {
+    public String grade(Student student){
+        if(student.average()>=90){
+            return "A+";
+        } else if (student.average()>=80) {
+            return "A";
+        } else if (student.average()>=75) {
+            return "B+";
+        } else if (student.average()>=70) {
+            return "B";
+        } else if (student.average()>=65) {
+            return "C+";
+        } else if (student.average()>=60) {
+            return "C";
+        } else if (student.average()>=55) {
+            return "D";
+        } else if (student.average()>=50) {
+            return "E";
+        }else{
+            return "Fail";
+        }
+    }
+}
