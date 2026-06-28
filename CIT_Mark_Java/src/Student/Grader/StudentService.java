bd1cf38cbcd84badf83b60fb08e2acff48663b72
sekibaalamark <markmajje256@gmail.com>
2026-06-28 16:00:14 +0300

    Configured JDBC on adding a Student

--- CIT_Mark_Java/src/Student/Grader/StudentService.java
+++ CIT_Mark_Java/src/Student/Grader/StudentService.java
@@ -1,15 +1,34 @@
 package Student.Grader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class StudentService {
+    private String url = "jdbc:postgresql://localhost:5432/cit_student_db";
+    private String username = "postgres";
+    private String password = "cit2026";
+    private Connection connection = DriverManager.getConnection(url,username,password);
     private HashMap<String,Student> studentHashMap = new HashMap<>();
+
+
+    public StudentService() throws SQLException {
+    }
 
     public HashMap<String, Student> getStudentHashMap() {
         return studentHashMap;
     }
 
-    public void add(String registrationNumber, Student student){
+    public void add(String registrationNumber, Student student) throws SQLException {
         studentHashMap.put(registrationNumber,student);
+        String insertStudentQuery = "INSERT INTO STUDENTS(name,registration_number) VALUES(?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(insertStudentQuery);
+        preparedStatement.setString(1,student.getName());
+        preparedStatement.setString(2,student.getRegistrationNumber());
+        preparedStatement.execute();
+        System.out.println(student.getName() + " " + student.getRegistrationNumber());
+
     }
 
     public String find(String registrationNumber){
