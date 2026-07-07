290e2aa7594dab116da4b654953ee49fea618090
atreavez <alvin69david@gmail.com>
2026-07-07 12:44:44 +0300

    Finised the java boolean challenge

---
+++ CiT-Java-Alvin/src/Learning/BooleanExamples.java
@@ -0,0 +1,13 @@
+package Learning;
+
+public class BooleanExamples {
+    public static void main(String[] args) {
+        int myAge = 22;
+        int votingAge = 18;
+        if (myAge >= votingAge) {
+            System.out.println("Old eneough to vote");
+        } else {
+            System.out.println("Young to vote");
+        }
+    }
+}
