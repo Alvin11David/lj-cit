08cb80931a2e32c037cf7eb6c40f8e2054340645
atreavez <alvin69david@gmail.com>
2026-07-07 12:08:38 +0300

    Learnt about random()

---
+++ CiT-Java-Alvin/src/Learning/Random.java
@@ -0,0 +1,8 @@
+package Learning;
+
+public class Random {
+    public static void main(String[] args) {
+        int randomNumber = (int) (java.lang.Math.random() * 1000);
+        System.out.println(randomNumber);
+    }
+}
