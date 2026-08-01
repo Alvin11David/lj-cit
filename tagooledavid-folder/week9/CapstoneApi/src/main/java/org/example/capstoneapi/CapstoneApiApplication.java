62ef1fd3e36f6a2a028f19c37cd3bb380a1c35db
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-08-01 11:37:56 +0300

    Initialize new project

---
+++ tagooledavid-folder/week9/CapstoneApi/src/main/java/org/example/capstoneapi/CapstoneApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.capstoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneApiApplication.class, args);
+    }
+
+}
---
+++ tagooledavid-folder/week9/CapstoneApi/src/main/java/org/example/capstoneapi/model/Student.java
@@ -0,0 +1,4 @@
+package org.example.capstoneapi.model;
+
+public class Student {
+}
